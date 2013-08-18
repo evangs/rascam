28b0614f9229f57c0fdfa7ce90b52dce5440878a
Evan Sailer <evan_gs@hotmail.com>
2013-08-18 17:07:46 -0500

    reverted changes and added file cleanup

--- server/camera.js
+++ server/camera.js
@@ -18,20 +18,22 @@ function sendImage(response, filename){
 
 function takeStill(response){
   // take a still photo and send it back in the response
-  //var date = new Date()
-  //var filename = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '-' + date.getTime() + '.jpg';
+  var date = new Date()
+  var filename = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '-' + date.getTime() + '.jpg';
   exec('raspistill -vf -t 0 -e jpg -q 75 -o - -w 640 -h 480', function (error, stdout, stderr) {
     if (error === null) {
-      //fs.exists(filename, function(exists){
-        //if (exists) {
+      fs.exists(filename, function(exists){
+        if (exists) {
           console.log('about to send image');
-          //sendImage(response, filename);
-          response.writeHead(200, {'Content-Type': 'image/jpeg'});
-          response.end(stdout, 'binary');
-        //} else {
-        //  status.notFound(response);
-        //}
-      //});
+          sendImage(response, filename);
+          fs.unlink(filename, function (err) {
+            if (err) throw err;
+            console.log('successfully deleted ' + filename);
+          });
+        } else {
+          status.notFound(response);
+        }
+      });
     } else {
       status.serverError(response);
     }
