b650d25ed528c36210ba52b205123a232ffea2c7
Evan Sailer <evan_gs@hotmail.com>
2013-08-18 17:02:42 -0500

    trying without writing to file

--- server/camera.js
+++ server/camera.js
@@ -18,14 +18,16 @@ function sendImage(response, filename){
 
 function takeStill(response){
   // take a still photo and send it back in the response
-  var date = new Date()
-  var filename = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '-' + date.getTime() + '.jpg';
-  exec('raspistill -vf -t 0 -e jpg -q 75 -o ' + filename + ' -w 640 -h 480', function (error, stdout, stderr) {
+  //var date = new Date()
+  //var filename = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '-' + date.getTime() + '.jpg';
+  exec('raspistill -vf -t 0 -e jpg -q 75 -o - -w 640 -h 480', function (error, stdout, stderr) {
     if (error === null) {
       fs.exists(filename, function(exists){
         if (exists) {
           console.log('about to send image');
-          sendImage(response, filename);
+          //sendImage(response, filename);
+          response.writeHead(200, {'Content-Type': 'image/jpeg'});
+          response.end(stdout, 'binary');
         } else {
           status.notFound(response);
         }
