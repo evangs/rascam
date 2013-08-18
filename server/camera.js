5562b2cfaebc508458512906155c943764199da5
Evan Sailer <evan_gs@hotmail.com>
2013-08-18 17:03:59 -0500

    Update camera.js

--- server/camera.js
+++ server/camera.js
@@ -22,16 +22,16 @@ function takeStill(response){
   //var filename = (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear() + '-' + date.getTime() + '.jpg';
   exec('raspistill -vf -t 0 -e jpg -q 75 -o - -w 640 -h 480', function (error, stdout, stderr) {
     if (error === null) {
-      fs.exists(filename, function(exists){
-        if (exists) {
+      //fs.exists(filename, function(exists){
+        //if (exists) {
           console.log('about to send image');
           //sendImage(response, filename);
           response.writeHead(200, {'Content-Type': 'image/jpeg'});
           response.end(stdout, 'binary');
-        } else {
-          status.notFound(response);
-        }
-      });
+        //} else {
+        //  status.notFound(response);
+        //}
+      //});
     } else {
       status.serverError(response);
     }
